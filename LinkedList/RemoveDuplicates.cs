63ea5a2eb1182eb7c22c2a4cd316adef94cd7e40
I Venkatesh <54239228+Venkatesh2510@users.noreply.github.com>
2026-04-09 23:21:40 +0530

    Add RemoveDuplicates method to linked list
    Implemented a method to remove duplicates from a sorted linked list.

---
+++ LinkedList/RemoveDuplicates.cs
@@ -0,0 +1,49 @@
+// 83. Remove Duplicates from Sorted List
+
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+// Example 2:
+
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        ListNode dummy=head;
+        while(dummy!=null)
+        {
+            while(dummy.next!=null&&dummy.val==dummy.next.val)
+            {
+                dummy.next=dummy.next.next;
+            }
+            dummy=dummy.next;
+        }
+        return head;
+    }
+}
