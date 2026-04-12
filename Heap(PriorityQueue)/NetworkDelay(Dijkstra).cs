a6ab278e5ad9577cfad85d4e4c1812cbe9e0575c
I Venkatesh <54239228+Venkatesh2510@users.noreply.github.com>
2026-04-12 11:38:54 +0530

    Add NetworkDelayTime method using Dijkstra's algorithm
    Implement Dijkstra's algorithm to calculate the minimum time for all nodes to receive a signal in a network.

---
+++ Heap(PriorityQueue)/NetworkDelay(Dijkstra).cs
@@ -0,0 +1,78 @@
+// 743. Network Delay Time
+
+
+// You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target.
+
+// We will send a signal from a given node k. Return the minimum time it takes for all the n nodes to receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
+
+ 
+
+// Example 1:
+
+
+// Input: times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+// Output: 2
+// Example 2:
+
+// Input: times = [[1,2,1]], n = 2, k = 1
+// Output: 1
+// Example 3:
+
+// Input: times = [[1,2,1]], n = 2, k = 2
+// Output: -1
+ 
+
+// Constraints:
+
+// 1 <= k <= n <= 100
+// 1 <= times.length <= 6000
+// times[i].length == 3
+// 1 <= ui, vi <= n
+// ui != vi
+// 0 <= wi <= 100
+// All the pairs (ui, vi) are unique. (i.e., no multiple edges.)
+
+public class Solution {
+    public int NetworkDelayTime(int[][] times, int n, int k) {
+        var graph=new List<(int,int)>[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            graph[i]=new List<(int,int)>();
+        }
+        foreach(var node in times)
+        {
+            int u=node[0];
+            int v=node[1];
+            int w=node[2];
+            graph[u].Add((v,w));
+        }
+        var dist = new int[n+1];
+        Array.Fill(dist, int.MaxValue);
+        var pq=new PriorityQueue<int,int>();
+        dist[k]=0;
+        pq.Enqueue(k,0);
+        while(pq.Count>0)
+        {
+            var curr = pq.Dequeue();
+            foreach(var (neighbour, time) in graph[curr])
+            {
+                int newDist=dist[curr]+time;
+                if(newDist<dist[neighbour])
+                {
+                    dist[neighbour]=newDist;
+                    pq.Enqueue(neighbour, dist[neighbour]);
+                }
+            }
+        }
+        int maxi=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(dist[i]==int.MaxValue)
+            {
+                return -1;
+            }
+            maxi=Math.Max(maxi, dist[i]);
+        }
+        return maxi;
+    }
+}
