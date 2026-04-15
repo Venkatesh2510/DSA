aec4c2deb441f540c4d985ef1bd1a0adcbf0b8dc
I Venkatesh <54239228+Venkatesh2510@users.noreply.github.com>
2026-04-15 19:31:21 +0530

    Add problem statement for swapping nodes in pairs
    Added problem statement for 'Swap Nodes in Pairs' including example input and output.

---
+++ Recursion/SwapNodeInPairs.cs
@@ -0,0 +1,33 @@
+24. Swap Nodes in Pairs
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+Example 1:
+
+Input: head = [1,2,3,4]
+
+Output: [2,1,4,3]
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode SwapPairs(ListNode head) {
+        if(head==null||head.next==null)
+        {
+            return head;
+        }
+        ListNode second=head.next;
+        head.next=SwapPairs(head.next.next);
+        second.next=head;
+        return second;
+    }
+}
