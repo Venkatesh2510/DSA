36bd886c4215155a6e20c5bef61ae40b55922320
I Venkatesh <54239228+Venkatesh2510@users.noreply.github.com>
2026-04-15 10:18:38 +0530

    Add ReverseList method for reversing linked list
    Implement ReverseList method to reverse a singly linked list iteratively.

---
+++ LinkedList/ReverseLinkedList.cs
@@ -0,0 +1,55 @@
+// 206. Reverse Linked List
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+// Example 2:
+
+
+// Input: head = [1,2]
+// Output: [2,1]
+// Example 3:
+
+// Input: head = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the list is the range [0, 5000].
+// -5000 <= Node.val <= 5000
+ 
+
+// Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode curr=head;
+        ListNode prev=null;
+        while(curr!=null)
+        {
+            ListNode TempNextNode=curr.next; //preserves next node
+            curr.next=prev; //breaks next pointer and assings it to previous
+            prev=curr; //moves prev pointer to current
+            curr=TempNextNode; //moves to current to next;
+        }
+        return prev;
+    }
+}
